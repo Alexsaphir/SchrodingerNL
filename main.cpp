003125d8ed2d26815ae42d03db9100c43c6b3f3b
Alexsaphir <alex.game@sfr.fr>
2016-09-09 14:10:32 +0200

    Delete all debug test

--- main.cpp
+++ main.cpp
@@ -5,29 +5,12 @@
 
 int main(int argc, char **argv)
 {
-	//QApplication app(argc, argv);
-	Solver1D W(-1.,1.,1.,0.,0.,.1);
+	QApplication app(argc, argv);
+	Solver1DGui W(-100.,100.,.1,0.,0.,.001);
 	W.initPulse();
-	qDebug() << "Value of Init Pulse : " << W.getValueNorm(0) << W.getValueNorm(1) << W.getValueNorm(2);
-
-	W.doStep();
-	qDebug() << "Value at Step 1 : " << W.getValueNorm(0) << W.getValueNorm(1) << W.getValueNorm(2);
-
-	W.doStep();
-	qDebug() << "Value at Step 2 : " << W.getValueNorm(0) << W.getValueNorm(1) << W.getValueNorm(2);
-
-	W.doStep();//Step 3
-	W.doStep();//Step 4
-	W.doStep();//Step 5
-	W.doStep();//Step 6
-	W.doStep();//Step 7
-	W.doStep();//Step 8
-	W.doStep();//Step 9
-	W.doStep();//Step 10
-	qDebug() << "Value at Step 2 : " << W.getValueNorm(0) << W.getValueNorm(1) << W.getValueNorm(2);
-
-	return 0;
-	//return app.exec();
+	W.refreshView();
+	W.show();
+	return app.exec();
 }
 
 
