688656c235fab221a043984157ca12537c9ac0fa
Alexandre <alex.game@sfr.fr>
2016-09-09 11:58:54 +0200

    Parameters set to test if Solver1D works

--- main.cpp
+++ main.cpp
@@ -5,15 +5,29 @@
 
 int main(int argc, char **argv)
 {
-	QApplication app(argc, argv);
-	Solver1DGui W(-100.,100.,.01,-10.,-10.,.00000001);
+	//QApplication app(argc, argv);
+	Solver1D W(-1.,1.,1.,0.,0.,.1);
 	W.initPulse();
-	W.refreshView();
-	W.show();
+	qDebug() << "Value of Init Pulse : " << W.getValueNorm(0) << W.getValueNorm(1) << W.getValueNorm(2);
 
+	W.doStep();
+	qDebug() << "Value at Step 1 : " << W.getValueNorm(0) << W.getValueNorm(1) << W.getValueNorm(2);
 
-	//return 0;
-	return app.exec();
+	W.doStep();
+	qDebug() << "Value at Step 2 : " << W.getValueNorm(0) << W.getValueNorm(1) << W.getValueNorm(2);
+
+	W.doStep();//Step 3
+	W.doStep();//Step 4
+	W.doStep();//Step 5
+	W.doStep();//Step 6
+	W.doStep();//Step 7
+	W.doStep();//Step 8
+	W.doStep();//Step 9
+	W.doStep();//Step 10
+	qDebug() << "Value at Step 2 : " << W.getValueNorm(0) << W.getValueNorm(1) << W.getValueNorm(2);
+
+	return 0;
+	//return app.exec();
 }
 
 
