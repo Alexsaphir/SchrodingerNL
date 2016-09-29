bb04e0313a4b0627bdfb5156f49cdd47c7371da2
Alexsaphir <alex.game@sfr.fr>
2016-09-29 15:38:45 +0200

    Add Grid2D class to support PDE in two spacial dimension

---
+++ include/grid2d.h
@@ -0,0 +1,58 @@
+#ifndef GRID2D_H
+#define GRID2D_H
+
+#include <QVector>
+
+#include "type.h"
+#include "axis.h"
+
+class Grid2D
+{
+public:
+	Grid2D(const Axis &X, const Axis &Y);
+
+	Type getDx() const;
+	Type getDy() const;
+	Type getPosX(int i, int j) const;
+	Type getPosY(int i, int j) const;
+	cmplx getValue(int i,int j) const;
+	Type getXmax() const;
+	Type getXmin() const;
+	Type getYmax() const;
+	Type getYmin() const;
+	int getNx() const;
+	int getNy() const;
+	void setValue(int i, int j, cmplx value);
+
+	bool isInGrid(int i, int j) const;
+
+private:
+	QVector<cmplx> V;
+
+	Axis AxisX;
+	Axis AxisY;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // GRID2D_H
---
+++ src/grid2d.cpp
@@ -0,0 +1,87 @@
+#include "include/grid2d.h"
+
+Grid2D::Grid2D(const Axis &X, const Axis &Y)
+{
+	AxisX = X;
+	AxisY = Y;
+	V.fill(cmplx(0.,0.),getNx()*getDy());
+}
+
+Type Grid2D::getDx() const
+{
+	return AxisX.getAxisStep();
+}
+
+Type Grid2D::getDy() const
+{
+	return AxisY.getAxisStep();
+}
+
+int Grid2D::getNx() const
+{
+	return AxisX.getAxisN();
+}
+
+int Grid2D::getNy() const
+{
+	return AxisY.getAxisN();
+}
+
+Type Grid2D::getPosX(int i, int j) const
+{
+	if (isInGrid(i, j))
+		return getDx()*(Type)i;
+	else
+		return (Type)0;
+}
+
+Type Grid2D::getPosY(int i, int j) const
+{
+	if (isInGrid(i, j))
+		return getDy()*(Type)j;
+	else
+		return (Type)0;
+}
+
+cmplx Grid2D::getValue(int i, int j) const
+{
+	//The domain look if (i,j) is in the grid or give a boundary condition
+	//We can skip the test if (i,j) is in the grid
+
+	return V.at(getNx()*j+i);
+}
+
+Type Grid2D::getXmax() const
+{
+	return AxisX.getAxisMax();
+}
+
+Type Grid2D::getXmin() const
+{
+	return AxisX.getAxisMin();
+}
+
+Type Grid2D::getYmax() const
+{
+	return AxisY.getAxisMax();
+}
+
+Type Grid2D::getYmin() const
+{
+	return AxisY.getAxisMin();
+}
+
+bool Grid2D::isInGrid(int i, int j) const
+{
+	if(i<0 || i>=getNx() || j<0 || j>=getNy())
+		return false;
+	else
+		return true;
+}
+
+void Grid2D::setValue(int i, int j, cmplx value)
+{
+	if (!isInGrid(i,j))
+		return;
+	V.replace(i+getNx()*j, value);
+}
