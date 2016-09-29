895f215f35ddef2065f80ac1d9fb8fa24b0f563e
Alexsaphir <alex.game@sfr.fr>
2016-09-29 15:28:46 +0200

    Create Type Header
    This header contains multiple typedef to simplify the code

---
+++ include/type.h
@@ -0,0 +1,14 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+#include <QPair>
+
+#include <complex>
+#include <cfloat>
+
+
+typedef double Type;
+typedef std::complex<Type> cmplx;
+typedef QPair<int, int> Position;
+
+#endif // TYPE_H
