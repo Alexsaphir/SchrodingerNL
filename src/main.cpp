955d055f6aa600f3b0549c2a887e89bb57318ff9
Alexsaphir <alex.game@sfr.fr>
2016-09-30 20:52:27 +0200

    Add Test instruction

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <QApplication>
+#include <QDebug>
 
-#include "include/grid1d.h"
 #include "include/solver1dgui.h"
 
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
+	Solver1DGui W(Axis(-10,10,.1),0,0,.001);
+	W.initPulse();
+	W.show();
+//	Axis X(-10,10,1);
+//	Grid1D G(X);
+
+//	qDebug() << X.getAxisMin() << G.getN();
+
 	return app.exec();
+	return 0;
 }
-
-
-
