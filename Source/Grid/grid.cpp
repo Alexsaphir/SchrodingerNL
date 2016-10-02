0e1ae2e2b6c35fe730e40c8f6f1ac7d3944d49c6
Alexsaphir <alex.game@sfr.fr>
2016-10-02 20:17:38 +0200

    Begin Grid Class to support multi dimensional Grid

--- Source/Grid/grid.cpp
+++ Source/Grid/grid.cpp
@@ -2,5 +2,37 @@
 
 Grid::Grid()
 {
-
+	isInit = false;
 }
+
+void Grid::AddAxis(const Axis &X)
+{
+	if(isInit)
+		return;
+	Axis *T;
+	T = new Axis(X);
+	Repere.append(T);
+}
+
+Type Grid::getStep(int AxisN) const
+{
+	if(AxisN >= Repere.size())
+		return Repere.at(AxisN)->getAxisStep();
+	else
+		return 0.;
+}
+
+int Grid::getIndexFromPos(const Point &Pos) const
+{
+	if(Repere.empty())
+		return -1;
+	if(Pos.Dim()!=Repere.size())
+		return -1;
+	int index(Pos.getValue(0));
+	for(int i=1; i<Repere.size(); ++i)
+	{
+		index+=Pos.getValue(i)*Repere.at(i-1)->getAxisN();
+	}
+	return index;
+}
+
--- Source/Grid/grid.h
+++ Source/Grid/grid.h
@@ -1,11 +1,35 @@
 #ifndef GRID_H
 #define GRID_H
 
+#include <QVector>
+
+#include "../axis.h"
+#include "../point.h"
+#include "../type.h"
+
 
 class Grid
 {
 public:
 	Grid();
+
+	void AddAxis(const Axis &X);
+	void initGrid();
+
+	Type getStep(int AxisN) const;
+
+	int getN() const;
+
+	cmplx getValue(const Point &Pos) const;
+	void setValue(const Point &Pos, cmplx value);
+
+private:
+	int getIndexFromPos(const Point &P) const;
+
+private:
+	QVector<Axis*> Repere;
+	QVector<cmplx> V;
+	bool isInit;
 };
 
 #endif // GRID_H
