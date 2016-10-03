c574f3dffa6db3d59ad5b3b0ac013fbb003760f1
Alexsaphir <alex.game@sfr.fr>
2016-10-03 16:58:39 +0200

    Add method to Grid Class

--- Source/Grid/grid.cpp
+++ Source/Grid/grid.cpp
@@ -3,6 +3,7 @@
 Grid::Grid()
 {
 	isInit = false;
+	N = 0;
 }
 
 void Grid::AddAxis(const Axis &X)
@@ -14,16 +15,10 @@ void Grid::AddAxis(const Axis &X)
 	Repere.append(T);
 }
 
-Type Grid::getStep(int AxisN) const
-{
-	if(AxisN >= Repere.size())
-		return Repere.at(AxisN)->getAxisStep();
-	else
-		return 0.;
-}
-
 int Grid::getIndexFromPos(const Point &Pos) const
 {
+	if(!isInit)
+		return -1;
 	if(Repere.empty())
 		return -1;
 	if(Pos.Dim()!=Repere.size())
@@ -35,4 +30,113 @@ int Grid::getIndexFromPos(const Point &Pos) const
 	}
 	return index;
 }
+
+int Grid::getAxisN() const
+{
+	if (!isInit)
+		return 0;
+	return Repere.size();
+}
+
+int Grid::getN() const
+{
+	if(!isInit)
+		return 0;
+	return N;
+}
+
+int Grid::getN(int AxisN) const
+{
+	if(!isInit)
+		return 0;
+	if(AxisN<0 || AxisN >= Repere.size())
+		return 0;
+	return Repere.at(AxisN)->getAxisN();
+}
+
+Type Grid::getStep(int AxisN) const
+{
+	if(!isInit)
+		return 0;
+	if(AxisN <0 || AxisN >= Repere.size())
+		return 0.;
+	return Repere.at(AxisN)->getAxisStep();
+}
+
+cmplx Grid::getValue(const Point &Pos) const
+{
+	if(!isInit)
+		return cmplx(0,0);
+	//It's the Domain class who catch the index error
+	return V.at(getIndexFromPos(Pos));
+}
+
+cmplx Grid::getValue(int i) const
+{
+	if(!isInit)
+		return cmplx(0,0);
+	//It's the Domain class who catch the index error
+	return V.at(i);
+}
+
+
+void Grid::initGrid()
+{
+	if(isInit)
+		return;//Cancel multiple initialisation
+	isInit = true;
+	N = 0;
+	//Compute the size of the Grid
+	for(int i=0; i<Repere.size(); ++i)
+	{
+		if (N == 0)
+		{
+			N+= Repere.at(i)->getAxisN();
+		}
+		else
+		{
+			N*= Repere.at(i)->getAxisN();
+		}
+	}
+
+	V.fill(cmplx(0,0), N);
+}
+
+bool Grid::isInGrid(const Point &Pos) const
+{
+	if(!isInit)
+		return false;
+	//We can check if each indice of the position are good for each Axis
+	//Or we can compute the indice of Pos in the grid and do a simply test
+
+	return isInGrid(getIndexFromPos(Pos));
+
+}
+
+bool Grid::isInGrid(int i) const
+{
+	if(!isInit)
+		return false;
+	if(i<0 || i>=N)
+		return false;
+	return true;
+}
+
+void Grid::setValue(const Point &Pos, cmplx value)
+{
+	if(!isInit)
+		return;
+	//It's the Domain class who catch the index error
+	setValue(getIndexFromPos(Pos), value);
+
+}
+
+void Grid::setValue(int i, cmplx value)
+{
+	if(!isInit)
+		return;
+	//It's the Domain class who catch the index error
+	V.replace(i, value);
+}
+
 
--- Source/Grid/grid.h
+++ Source/Grid/grid.h
@@ -14,22 +14,28 @@ public:
 	Grid();
 
 	void AddAxis(const Axis &X);
-	void initGrid();
-
-	Type getStep(int AxisN) const;
-
+	int getAxisN() const;
+	int getIndexFromPos(const Point &Pos) const;
+	int getN(int AxisN) const;
 	int getN() const;
-
+	Type getStep(int AxisN) const;
 	cmplx getValue(const Point &Pos) const;
+	cmplx getValue(int i) const;
+	void initGrid();
+	bool isInGrid(const Point &Pos) const;
+	bool isInGrid(int i) const;
 	void setValue(const Point &Pos, cmplx value);
+	void setValue(int i, cmplx value);
 
-private:
-	int getIndexFromPos(const Point &P) const;
+
+
+
 
 private:
 	QVector<Axis*> Repere;
 	QVector<cmplx> V;
 	bool isInit;
+	int N;//Precomputed size of V
 };
 
 #endif // GRID_H
