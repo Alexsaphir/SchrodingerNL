c3c6782c7ea8d6d7df64d49c76642a1f062a387b
Alexsaphir <alex.game@sfr.fr>
2016-10-08 18:48:43 +0200

    Create type.cpp and update type.h

--- Source/type.h
+++ Source/type.h
@@ -1,14 +1,17 @@
 #ifndef TYPE_H
 #define TYPE_H
 
+#include <QDebug>
+
 #include <QPair>
+#include <QString>
 
 #include <complex>
-#include <cfloat>
-
 
 typedef double Type;
 typedef std::complex<Type> cmplx;
 typedef QPair<int, int> Position;
+
+QDebug operator<< (QDebug dbg, const cmplx &z);
 
 #endif // TYPE_H
