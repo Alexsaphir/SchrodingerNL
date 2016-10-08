a2bac3cd2d3cdcab97a87a203f63c47a9e43e3ba
Alexsaphir <alex.game@sfr.fr>
2016-10-08 23:38:25 +0200

    Add ColumnDataProxy to use a Domain Class as a ColumnMatrix

---
+++ Source/Matrix/Matrix/ColumnDataProxy/columndataproxy.cpp
@@ -0,0 +1,6 @@
+#include "columndataproxy.h"
+
+ColumnDataProxy::ColumnDataProxy()
+{
+
+}
---
+++ Source/Matrix/Matrix/ColumnDataProxy/columndataproxy.h
@@ -0,0 +1,12 @@
+#ifndef COLUMNDATAPROXY_H
+#define COLUMNDATAPROXY_H
+
+#include
+
+class ColumnDataProxy
+{
+public:
+	ColumnDataProxy();
+};
+
+#endif // COLUMNDATAPROXY_H
