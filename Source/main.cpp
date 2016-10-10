d1d7a2b868aba769732408c3f3ffcf0b7015dec1
Alexsaphir <alex.game@sfr.fr>
2016-10-10 16:01:50 +0200

    test main

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,8 +5,8 @@
 #include "Matrix/SparseMatrix/sparsematrix.h"
 #include "Matrix/Matrix/matrix.h"
 #include "Matrix/corematrix.h"
-#include "Matrix/Matrix/columnmatrix.h"
-#include "Matrix/Matrix/DataProxy/columndataproxy.h"
+#include "Matrix/Matrix/ColumnMatrix/columnmatrix.h"
+#include "Matrix/Matrix/ColumnMatrix/DataProxy/columndataproxy.h"
 
 int main(int argc, char **argv)
 {
@@ -15,25 +15,25 @@ int main(int argc, char **argv)
 	Domain *D = new Domain(cmplx(0,0));
 	D->AddAxis(Axis(-1,1,1));
 	D->AddAxis(Axis(-1,.1,1));
+	D->initGrid();
 
-
-	QVector<CoreMatrix*> CM;
+	QVector<ColumnMatrixVirtual*> CM;
 	//CM.push_back(new SparseMatrix(3,3));
 	//CM.push_back(new Matrix(3,3));
 
-	CM.push_back(new ColumnDataProxy);
+	CM.push_back(new ColumnDataProxy(D));
 
 
 
-	CM.at(0)->setValue(0,0,cmplx(.2,1));
+	CM.at(0)->setValue(0,1,cmplx(.2,1));
 	//CM.at(1)->setValue(0,0,cmplx(10,10));
 
 
 
-	qDebug().noquote() << CM.at(0)->getValue(0, 0) << CM.at(1)->getValue(0, 0);
+	qDebug().noquote() << CM.at(0)->at(0) << CM.at(0)->getValue(0, 1);
 
 	delete CM.at(0);
-	delete CM.at(1);
+//	delete CM.at(1);
 
 	return 0;
 }
