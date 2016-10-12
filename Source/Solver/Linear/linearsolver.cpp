c1794a1c7d2bec50de29b77717bedc517eb56ff2
Alexsaphir <alex.game@sfr.fr>
2016-10-13 00:03:57 +0200

    Implement the SOR solver in the Linear Class and an initialization of the sparse matrix for the heat equation
    The initSolver method  need improvement to support other PDE than the Heat Equation

--- Source/Solver/Linear/linearsolver.cpp
+++ Source/Solver/Linear/linearsolver.cpp
@@ -5,14 +5,57 @@ LinearSolver::LinearSolver(): Solver()
 	System = new SparseMatrix(10,10);
 }
 
-LinearSolver::LinearSolver(SparseMatrix *M)
+LinearSolver::LinearSolver(uint size)
 {
-	System = M;
+	System = new SparseMatrix(size,size);
 }
 
-void LinearSolver::initSolver()
-{
 
+void LinearSolver::initSolver(Type dt, Type dx)
+{
+	if(!System)
+		return;
+	Type alpha=dt/dx/dx;
+	System->setValue(0, 0, 1.);
+	for(uint i(1); i<(System->row()-1); ++i)
+	{
+		System->setValue(i, i, 1.+2.*alpha);
+		System->setValue(i, i-1, -alpha);//uint => 0-1==2^32
+		System->setValue(i, i+1, -alpha);//if out of order, it's catch by the sparseMatrix
+	}
+	System->setValue(System->row()-1, System->row()-1, 1.);
+}
+
+void LinearSolver::SORMethod(const ColumnMatrixVirtual *B, ColumnMatrixVirtual *X)
+{
+	uint n(System->row());
+	Type w(1.5);
+	//set X to 0
+	for(uint i(0); i<n; ++i)
+		X->set(i,0);
+
+	int step(0);
+	while(step!=50)
+	{
+		for(uint i=0;i<n;++i)
+		{
+			cmplx sigma(0,0);
+			for(uint j=0;j<n;++j)
+			{
+				if (j!=i)
+				{
+					sigma+=System->getValue(i,j)*X->at(j);
+				}
+			}
+			cmplx Res=B->at(i)-sigma;
+			Res/= System->getValue(i,i);
+			Res-=X->at(i);
+			Res*=w;
+			Res+=X->at(i);
+			X->set(i, Res);
+		}
+		++step;
+	}
 }
 
 
--- Source/Solver/Linear/linearsolver.h
+++ Source/Solver/Linear/linearsolver.h
@@ -3,13 +3,17 @@
 
 #include "../solver.h"
 #include "../../Matrix/SparseMatrix/sparsematrix.h"
+#include "../../Matrix/Matrix/ColumnMatrix/columnmatrixvirtual.h"
 
 class LinearSolver: public Solver
 {
 public:
 	LinearSolver();
-	LinearSolver(SparseMatrix *M);
-	virtual void initSolver();
+	LinearSolver(uint size);
+
+	virtual void initSolver(Type dt, Type dx);
+
+	void SORMethod(const ColumnMatrixVirtual *B, ColumnMatrixVirtual *X);
 
 	virtual ~LinearSolver();
 private:
