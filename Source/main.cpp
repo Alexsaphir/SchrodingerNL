07ad84eecb4fead793c3ec6c1702c605f7306800
Alexsaphir <alex.game@sfr.fr>
2016-10-13 22:05:11 +0200

    Try to test the linear solver with SOR algorithm

--- Source/main.cpp
+++ Source/main.cpp
@@ -11,36 +11,21 @@
 #include "Matrix/MatrixAlgorithm/matrixalgorithm.h"
 
 #include "Solver/Linear/linearsolver.h"
+#include "SolverGui/pdegui1d.h"
 
 int main(int argc, char **argv)
 {
-	ColumnMatrix *B = new ColumnMatrix(5);
+	QApplication app(argc, argv);
 
-	ColumnMatrix *X = new ColumnMatrix(5);
+	PDEGui1D *F;
+	F= new PDEGui1D();
+	F->show();
+	F->refreshView();
 
-	B->set(0,1);//BoundInf
-	B->set(2,3);
-	B->set(4,1);
 
-	LinearSolver LS(5);
-	LS.initSolver(10,.01);
-
-	int nTime(0);
-	qDebug() << B->at(0).real() << B->at(1).real() << B->at(2).real() << B->at(3).real() << B->at(4).real();
-	while(nTime<=10)
-	{
-		LS.SORMethod(B,X);
-		qDebug() << X->at(0).real() << X->at(1).real() << X->at(2).real() << X->at(3).real() << X->at(4).real();
-		nTime++;
-		//Permut Pointeur
-		ColumnMatrix *tmp;
-		tmp=B;
-		B=X;
-		X=tmp;
-
-	}
+	return app.exec();
 
 
 
-	return 0;
+
 }
