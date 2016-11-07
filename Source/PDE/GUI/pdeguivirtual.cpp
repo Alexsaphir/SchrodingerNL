678c76564041b9dc3ecf56efb435e72a34f00a0d
Alexsaphir <alex.game@sfr.fr>
2016-11-07 18:20:00 +0100

    Create a Class to display the Grid of a PDE problem

--- Source/PDE/GUI/pdeguivirtual.cpp
+++ Source/PDE/GUI/pdeguivirtual.cpp
@@ -1,6 +1,45 @@
 #include "pdeguivirtual.h"
 
-PDEGuiVirtual::PDEGuiVirtual()
+PDEGuiVirtual::PDEGuiVirtual(): QMainWindow()
 {
+	Problem = NULL;
 
+	SDI_Area = new QWidget;
+	GridLayout = new QGridLayout;
+
+	SDI_Area->setLayout(GridLayout);
+	setCentralWidget(SDI_Area);
+
+	setWindowTitle("PDE numerical simulation ");
+	this->resize(800,800);
+}
+
+bool PDEGuiVirtual::eventFilter(QObject *obj, QEvent *event)
+{
+	if (event->type() == QEvent::GraphicsSceneWheel)
+	{
+		Zoom(static_cast<QGraphicsSceneWheelEvent*> (event));
+		event->accept();//On ne propage pas l'event
+		return true;
+	}
+	if (event->type()==QEvent::KeyPress)
+	{
+		QKeyEvent* key = static_cast<QKeyEvent*>(event);
+		if ( (key->key()==Qt::Key_Enter) )
+		{
+			//Enter or return was pressed-
+			if (!Problem)
+				Problem->computeNextStep();
+		}
+		refreshView();
+		return true;
+	}
+	return false;
+}
+
+
+PDEGuiVirtual::~PDEGuiVirtual()
+{
+	delete GridLayout;
+	delete SDI_Area;
 }
--- Source/PDE/GUI/pdeguivirtual.h
+++ Source/PDE/GUI/pdeguivirtual.h
@@ -1,11 +1,36 @@
 #ifndef PDEGUIVIRTUAL_H
 #define PDEGUIVIRTUAL_H
 
+#include <QEvent>
+#include <QGraphicsSceneWheelEvent>
+#include <QGridLayout>
+#include <QKeyEvent>
+#include <QMainWindow>
 
-class PDEGuiVirtual
+#include "../pdevirtual.h"
+
+
+class PDEGuiVirtual: public QMainWindow
 {
 public:
 	PDEGuiVirtual();
+	~PDEGuiVirtual();
+
+public slots:
+	virtual void refreshView() = 0;
+protected:
+	virtual bool eventFilter(QObject *obj, QEvent *event);
+protected:
+	virtual void Zoom(QGraphicsSceneWheelEvent *event) = 0;
+
+protected:
+	PDEVirtual *Problem;
+
+
+	QGridLayout *GridLayout;
+	//QScrollArea *Scroll;
+	QWidget *SDI_Area;
+
 };
 
 #endif // PDEGUIVIRTUAL_H
