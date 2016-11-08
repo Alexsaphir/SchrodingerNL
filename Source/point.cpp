4e223c3d5d8bb9d2a86b358e1adec7f1af6ce8b7
Alexsaphir <alex.game@sfr.fr>
2016-11-08 22:27:31 +0100

    Update Point Class

--- Source/point.cpp
+++ Source/point.cpp
@@ -2,43 +2,56 @@
 
 Point::Point()
 {
-	V.append(0);
+	m_V.append(0);
+}
+
+Point::Point(QVector<int> V): m_V(V)
+{
+}
+
+Point::Point(const Point &P): m_V(P.m_V)
+{//Copy constructor
 }
 
 Point::Point(int x)
 {
-	V << x;
+	m_V << x;
 }
 
 Point::Point(int x, int y)
 {
-	V << x << y;
+	m_V << x << y;
 }
 
 Point::Point(int x, int y, int z)
 {
-	V << x << y << z;
+	m_V << x << y << z;
 }
 
 int Point::Dim() const
 {
-	return V.size();
+	return m_V.size();
+}
+
+int Point::at(int i) const
+{
+	return  m_V.at(i);
 }
 
 int Point::getValue(int i) const
 {
-	if (i<0 || i>=V.size())
+	if (i<0 || i>=m_V.size())
 		return 0.;
-	return V.at(i);
+	return m_V.at(i);
 }
 
 void Point::setValue(int i, int Coord)
 {
 	if(i<0)
 		return;
-	while(V.size()<i+1)//Test if there are enought item in the QVector
-		V.append(0);
-	V.replace(i, Coord);
+	while(m_V.size()<i+1)//Test if there are enought item in the QVector
+		m_V.append(0);
+	m_V.replace(i, Coord);
 }
 
 int Point::x() const
--- Source/point.h
+++ Source/point.h
@@ -9,6 +9,8 @@ class Point
 {
 public:
 	Point();
+	Point(QVector<int> V);
+	Point(const Point &P);
 	Point(int x);
 	Point(int x, int y);
 	Point(int x, int y, int z);
@@ -16,12 +18,19 @@ public:
 	int y() const;//V.at(1)
 	int z() const;
 
+	int at(int i) const;
 	int getValue(int i) const;
 	int Dim() const;
 	void setValue(int i, int Coord);
 
+	//bool isIn(const Grid *G);
+
 private:
-	QVector<int> V;
+	QVector<int> m_V;
 };
 
 #endif // POINT_H
+
+//The class Point describe a position on the grid.
+//The grid can be see as a multidimensional array.
+//But negative position permit to access to boundary conditions
