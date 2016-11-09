0e8bbd9d920f7ff86aa9cdd28a0fd9c7c8dc607c
Alexsaphir <alex.game@sfr.fr>
2016-11-09 22:57:48 +0100

    We can now begin to try to simulate schrodinger in 1D

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,56 +1,33 @@
+#include <QApplication>
 #include <QDebug>
 
-#include "Solver/Linear/linearsolver.h"
 #include "Axis/linearaxis.h"
-#include "Domain/Base/domainbase.h"
-#include "Matrix/Matrix/ColumnMatrix/DataProxy/columndataproxy.h"
-#include "Matrix/Matrix/ColumnMatrix/columnmatrix.h"
-#include "debugclass.h"
+#include "PDE/Linear/1D/schrodinger1d.h"
+#include "PDE/GUI/1D/pdegui1d.h"
 
 int main(int argc, char **argv)
 {
-	Axis *axe;
-	axe = new LinearAxis(-1,1,1);
-	Frame *F;
-	F = new Frame(axe);
+	QApplication app(argc, argv);
 
-	ColumnMatrix *B;
-	ColumnMatrix *X;
-	B = new ColumnMatrix(3);
-	X = new ColumnMatrix(3);
+	Schrodinger1D *PDE_Schrodinger = NULL;
+	PDEGui1D *Gui_Schrodinger = NULL;
+	Axis *Z = NULL;
 
-	DomainBase *B_DB;
-	DomainBase *X_DB;
-	B_DB = new DomainBase(F, cmplx(0,0));
+	Z = new LinearAxis(-50,50,.1);
+
+	PDE_Schrodinger = new Schrodinger1D(Z, 0, 1, 1);
+	PDE_Schrodinger->initializeLinearSolver();
+	PDE_Schrodinger->InitialState();
+
+	Gui_Schrodinger = new PDEGui1D(PDE_Schrodinger);
 
 
+	Gui_Schrodinger->show();
 
-	LinearSolver *LS;
-	LS = new LinearSolver(3);
 
-	SparseMatrix *SM(LS->getSparseMatrix());
-	SM->setValue(0, 0, 2);
-	SM->setValue(0, 1, -1);
-	SM->setValue(1, 0, 1);
-	SM->setValue(1, 1, 5);
-	SM->setValue(2,2,1);//If we remove this line we can't find a solution
-	//The problem in Schrodinger1D it's maybe a bug like this
-	SM = NULL;
+	//delete PDE_Schrodinger;
+	//delete Gui_Schrodinger;
+	//delete Axe;
 
-	B->set(0,3);
-	B->set(1,1);
-
-	LS->SORMethod(B, X);
-
-	qDebug() << X->at(0) << X->at(1) << X->at(2);
-
-	delete axe;
-	delete F;
-	delete B;
-	delete X;
-	delete LS;
-	delete B_DB;
-	delete X_DB;
-
-	return 0;
+	return app.exec();
 }
