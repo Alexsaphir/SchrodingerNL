226da6955ab7f85ff1209fdbbd1fdc3de8466522
Alexsaphir <alex.game@sfr.fr>
2017-01-01 20:16:25 +0100

    Add a function to evaluate  f and the Jacobian of f

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,11 +2,148 @@
 #include <QDebug>
 
 #include "Axis/linearaxis.h"
+#include "debugclass.h"
+#include "Grid/gridmanager.h"
+#include "Gui/pdegui1d.h"
+#include "Matrix/SparseMatrix/sparsematrix.h"
+
+const Type dt(.1);
+const Type dx(1);
+const cmplx alpha(0,dt/dx/dx);
+
+
+void f(const GridBase *X, GridBase *Y, GridManager *Data)
+{
+	//Compute f(X)=Y
+	//We assume that the size of X and Y are equal
+
+	const int N(X->getSizeOfAxis(0));
+	const cmplx i(0,1);
+	//Left-BC
+	Y->setValue(0,cmplx(0,0));
+	for(int k=1;k<(N-1);++k)
+	{
+		cmplx tmp(0,0);
+
+		tmp+=1.;
+		tmp+=2.*alpha;
+		tmp-=i*dt*std::norm(X->getValue(k));
+		tmp*=X->getValue(k);
+
+		tmp-=X->getValue(k-1)*alpha;
+
+		tmp-=X->getValue(k+1)*alpha;
+
+		tmp-=Data->getValue(k, 0);//Value at k at the current time
+
+		Y->setValue(k, tmp);
+	}
+
+	//Right-BC
+	Y->setValue(N-1,cmplx(N-1,0));
+}
+
+void showGrid(GridBase *X , QApplication *app)
+{
+	PDEGui1D *Fen;
+	Fen = new PDEGui1D(X);
+	Fen->show();
+	app->exec();//wait until Fen is close
+	delete Fen;//Fen can be deleted
+}
+
+void ComputeJacobian(const GridBase *X,SparseMatrix *Jac, GridManager *Data)
+{
+	//Compute the jacobian in X
+	int N=X->getSizeOfAxis(0);
+	for(int i=0; i<N; ++i)
+	{
+		if((i-1)>=0)
+		{
+			//Compute derivative in x_(i-1)
+			Jac->setValue(i, i-1, -alpha);
+		}
+		if((i+1)<N)
+		{
+			//Compute derivative in x_(i+1)
+			Jac->setValue(i, i+1, -alpha);
+		}
+		//Compute derivative in x_i
+
+		cmplx tmp(0,0);
+		const cmplx dti(0,dt);
+		tmp+=1.;
+		tmp+=2.*alpha;
+		tmp+=dti*std::norm(X->getValue(i));
+		tmp+=-X->getValue(i)*dti*2.*std::conj(X->getValue(i));
+		Jac->setValue(i, i, tmp);
+	}
+}
+
+void LinearSolver(const SparseMatrix *A)
+{
+
+}
 
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
+	//Create the Frame with only one dimension
+	Axis *X;
+	X = new LinearAxis(-5., 5.,dx);
+
+	const int N(X->getAxisN());
+
+	Frame *F;
+	F = new Frame(X);
+
+	//Create the GridManager with 1 current time and 1 future time
+	GridManager *Manager;
+	Manager = new GridManager(0,1,*F);
+
+	//Init the Current Grid with the pulse
+	for(int i=0; i<F->getAxis(0)->getAxisN(); ++i)
+	{
+		Type x = F->getAxis(0)->getAxisMin()+F->getAxis(0)->getAxisStep()*(Type)(i);
+		cmplx w(0,100.*x);
+		Manager->getCurrentGrid()->setValue(i,std::exp(-x*x/4.)*std::exp(w));
+	}
+
+	//Show the initial state aka the current grid
+	showGrid(Manager->getCurrentGrid(), &app);
 
 
-	return app.exec();
+	//Compute the second step
+
+	//Initial Guess x0
+	GridBase *InitialGuess;
+	InitialGuess = Manager->getCurrentGrid();
+
+	//Create the Jacobian of the system
+	SparseMatrix *Jac;
+	Jac = new SparseMatrix(N, N);
+	//Compute the Jacobian with Initial guess value
+	ComputeJacobian(InitialGuess, Jac, Manager);
+
+	//Compute f(x0) and stock the value in temporary Grid
+	GridBase *B;
+	B = new GridBase(F);
+	f(InitialGuess, B, Manager);
+
+
+
+
+
+
+
+
+
+	delete B;
+	delete Jac;
+	delete Manager;
+	delete F;
+	delete X;
+
+	return 0;
+	//return app.exec();
 }
