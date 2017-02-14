5d1145706cb1dd0d4a0c3c317c33727d98f326c6
Alexsaphir <alex.game@sfr.fr>
2017-02-14 17:48:14 +0100

    Remove script file

--- CUDA/SchrodingerSplitStepFFT/plotscript.sh
+++
@@ -1,1 +0,0 @@
-do for [i=0:10] { plot sprintf('data%d.ds', i) using 1:2 w l; pause 0.5 }
--- CUDA/SchrodingerSplitStepFFT/video.sh
+++
@@ -1,6 +0,0 @@
-for i in {1..500}
-do
-	gnuplot -e "set terminal jpeg; splot 'data$i.ds' " > pic$i.jpeg 
-done
-
-ffmpeg -i pic%04d.jpeg movie.mpeg
