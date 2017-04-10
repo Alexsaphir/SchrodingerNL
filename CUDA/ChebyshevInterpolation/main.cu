ae1716b1358f6691a7d5e20bbb9e02548a1a60b5
Alexsaphir <alex.game@sfr.fr>
2017-04-10 20:19:09 +0200

    Create a project to implement a Chebyshev interpolation

---
+++ CUDA/ChebyshevInterpolation/main.cu
@@ -0,0 +1,23 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <device_functions.h>
+#include <device_launch_parameters.h>
+
+#include <cuda_runtime_api.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+
+#define N 32
+
+
+
+int main()
+{
+
+	return 0;
+}
