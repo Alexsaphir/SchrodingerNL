dde65a8f9bfa481ce258e69b4fbee0164ba6e5bf
Alexsaphir <alex.game@sfr.fr>
2017-04-22 16:23:57 +0200

    Rename main.cu to main.cpp
    No GPU code on the main

--- CUDA/PseudoSpectral/main.cpp
+++ CUDA/PseudoSpectral/main.cpp
@@ -1,11 +1,3 @@
-#include <cuda.h>
-#include <cufft.h>
-#include <cuda_runtime.h>
-#include <device_functions.h>
-#include <device_launch_parameters.h>
-
-#include <cuda_runtime_api.h>
-
 #include <iostream>
 #include <fstream>
 #include <string>
