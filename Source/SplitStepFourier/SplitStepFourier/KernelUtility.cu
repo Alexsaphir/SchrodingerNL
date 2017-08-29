43e913b8453897d8f394a468fdbc5f2ae99bee76
Alexsaphir <alex.game@sfr.fr>
2017-08-29 20:23:23 +0200

    Add utility to compute index and the number of grid for 2D grid and 2D block

--- Source/SplitStepFourier/SplitStepFourier/KernelUtility.cu
+++ Source/SplitStepFourier/SplitStepFourier/KernelUtility.cu
@@ -1,5 +1,12 @@
 #include "KernelUtility.cuh"
 
+__device__ int getGlobalIndex_2D_2D()
+{
+	int BlockId = blockIdx.x + blockIdx.y*gridDim.x;
+	return BlockId*(blockDim.x*blockDim.y) + (threadIdx.y*blockDim.x) + threadIdx.x;//ThreadId
+}
+
+
 int KernelUtility::computeNumberOfBlocks(int nbThreadPerBlock, int nbThread)
 {
 	return ((nbThread % nbThreadPerBlock) == 0) ? (nbThread / nbThreadPerBlock) : (1 + nbThread / nbThreadPerBlock);
@@ -8,4 +15,12 @@ int KernelUtility::computeNumberOfBlocks(int nbThreadPerBlock, int nbThread)
 int KernelUtility::computeNumberOfBlocks(int nbThread)
 {
 	return KernelUtility::computeNumberOfBlocks(1024, nbThread);
+}
+
+dim3 KernelUtility::computeNumberOfBlocks2D(dim3 blockSize, int Nx, int Ny)
+{
+	dim3 gridSize;
+	gridSize.x = computeNumberOfBlocks(blockSize.x, Nx);
+	gridSize.y = computeNumberOfBlocks(blockSize.y, Ny);
+	return gridSize;
 }
--- Source/SplitStepFourier/SplitStepFourier/KernelUtility.cuh
+++ Source/SplitStepFourier/SplitStepFourier/KernelUtility.cuh
@@ -7,9 +7,13 @@
 #include <device_launch_parameters.h>
 #include <cuda_runtime_api.h>
 
+__device__ int getGlobalIndex_2D_2D();
+
 namespace KernelUtility
 {
 	int computeNumberOfBlocks(int nbThreadPerBlock, int nbThread);
 	int computeNumberOfBlocks(int nbThread);
+
+	dim3 computeNumberOfBlocks2D(dim3 blockSize, int Nx, int Ny);
 }
 
