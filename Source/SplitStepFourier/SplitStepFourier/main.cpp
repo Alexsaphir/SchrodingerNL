e930701df7e3c2cf04f60c8951b3d1219bb5573e
Alexsaphir <alex.game@sfr.fr>
2017-09-02 23:04:00 +0200

    Test the SplitStepSolver2D

--- Source/SplitStepFourier/SplitStepFourier/main.cpp
+++ Source/SplitStepFourier/SplitStepFourier/main.cpp
@@ -5,7 +5,11 @@
 #include <string>
 
 #include "Grid.cuh"
+#include "Grid2D.cuh"
+
 #include "SplitStepSolver.cuh"
+#include "SplitStepSolver2D.cuh"
+
 #include "NLSUtility.h"
 #include "ExportData.h"
 
@@ -15,27 +19,37 @@
 //2*M_PI
 #define M_PI2 6.2831853071795864769252867665590057683943387987502
 
-#define N_FFT 4096//Frequency Sampling*Duration
+#define N_FFT 2048//Frequency Sampling*Duration
 
 
 int main()
 {
-	int N;
+	int Nx;
 	double Xmin;
 	double Xmax;
+	
+	int Ny;
+	double Ymin;
+	double Ymax;
+	
 	double dt;
 
 	int For1;
 	int For2;
 
-	N = N_FFT;
-	Xmin = -2;
-	Xmax = 2;
-	dt = .001;
+	Nx = N_FFT;
+	Xmin = -20;
+	Xmax = 20;
+
+	Ny = N_FFT;
+	Ymin = -20;
+	Ymax = 20;
+
+	dt = .0001;
 
 	//For1 = 100000;
-	For1 = 100;
-	For2 = 50;
+	For1 = 50;
+	For2 = 500;
 
 	bool b_demo(true);
 	//b_demo = false;
@@ -54,10 +68,10 @@ int main()
 	{
 		std::cin >> Xmax;
 	}
-	std::cout << "\nN :";
+	std::cout << "\nNx :";
 	if (!b_demo)
 	{
-		std::cin >> N;
+		std::cin >> Nx;
 	}
 	std::cout << "\ndt :";
 	if (!b_demo)
@@ -76,55 +90,42 @@ int main()
 	}
 
 	std::cout << "\n\n\nAxis X :";
-	std::cout << "\n\t[Xmin , Xmax] , N :[" << Xmin << " , " << Xmax << "] , " << N;
-	std::cout << "\n\tSpatial resolution :" << (Xmax - Xmin) / static_cast<double>(N);
-	std::cout << "\n\tFrequency resolution :"<< 2.*M_PI / static_cast<double>(N);
+	std::cout << "\n\t[Xmin , Xmax] , Nx :[" << Xmin << " , " << Xmax << "] , " << Nx;
+	std::cout << "\n\tSpatial resolution :" << (Xmax - Xmin) / static_cast<double>(Nx);
+
+	std::cout << "\n\n\nAxis Y :";
+	std::cout << "\n\t[Ymin , Ymax] , Ny :[" << Ymin << " , " << Ymax << "] , " << Ny;
+	std::cout << "\n\tSpatial resolution :" << (Ymax - Ymin) / static_cast<double>(Ny);
+
+	std::cout << "\n\tX-Frequency resolution :"<< 2.*M_PI / static_cast<double>(Nx);
+	std::cout << "\n\tY-Frequency resolution :" << 2.*M_PI / static_cast<double>(Ny);
 	std::cout << "\nTime resolution :" << static_cast<double>(For2)*dt;
 	std::cout << "\nTotal time :" << static_cast<double>(For2*For1)*dt;
 	std::cout << "\n\n\nBegin computing!\n";
 	
 	//FFT plan
 	cufftHandle plan;
-	cufftPlan1d(&plan, N, CUFFT_Z2Z, 1);
 	
-	Grid *S = new Grid(Xmin, Xmax, N);
-	NLSUtility::GaussPulseLinear(S, 50, .5, -6, -60);
 	
-	//Shift pulse +25
-
-	//Shift pulse with add -50
-	//S->syncDeviceToHost();
-
-	//std::vector<cmplx> A;
-	//std::vector<cmplx> B;
-
-	//for (int i = 0; i < N; ++i)
-	//{
-		//A.push_back(S->getHostData()[(i + N / 4) % N]);
-		//B.push_back(S->getHostData()[(i + 3 * N / 4) % N]);
-		//std::cout << (i + N / 2) % N << "\t" << (i + 3 * N / 2) % N << "\n";
-	//}
-	//for (int i = 0; i < N; ++i)
-	//{
-		//S->getHostData()[i] = cuCexp(iMul(-M_PI))*A.at(i) + B.at(i);
-	//}
+	//cufftPlan1d(&plan, Nx, CUFFT_Z2Z, 1);
+	//Grid *S = new Grid(Xmin, Xmax, Nx);
+	//NLSUtility::GaussPulseLinear(S, 50, .5, -6, -60);
 	
-
-	//A.clear();
-	//B.clear();
-	
-	//S->syncHostToDevice();
+	cufftPlan2d(&plan, Nx, Ny, CUFFT_Z2Z);
+	Grid2D *S = new Grid2D(Xmin, Xmax, Nx, Ymin, Ymax, Ny);
+	NLSUtility::GaussMonoPulse2D(S);
 
 	double t(0);
 	
 	for (int i = 0; i <= For1; ++i)
 	{
 		//exportData(S, "Plot/data" + std::to_string(i) + ".ds");
-		exportCsvXY(S, "B:/data" + std::to_string(i) + ".csv");
-		exportCsvXTY(S, "Plot/dataT.csv", i);
-		//exportCsv2DMatlab(S, "B:/Matlab.csv", i);
-		double M = NLSUtility::computeTotalMass(S);
-		exportMassOverTime(M, "B:/dataE.csv", i);
+		//exportCsvXY(S, "B:/data" + std::to_string(i) + ".csv");
+		//exportCsvXTY(S, "Plot/dataT.csv", i);
+		exportCsv2DMatlab(S, "B:/Matlab.csv", i);
+		double M = 0;
+		//M = NLSUtility::computeTotalMass(S);
+		//exportMassOverTime(M, "B:/dataE.csv", i);
 
 		
 		std::cout << "Time :" << t << "\tM :" << M << std::endl;
@@ -132,7 +133,9 @@ int main()
 			break;
 		for (int j = 0; j < For2; ++j)
 		{
-			SplitStep(S->getDeviceData(), dt, N_FFT, Xmax - Xmin, &plan,1);
+			//SplitStep(S->getDeviceData(), dt, N_FFT, Xmax - Xmin, &plan,1);
+			SplitStep2D(S->getDeviceData(), dt, Nx*Ny, Nx, Ny, Xmax - Xmin, Ymax - Ymin, &plan, 1);
+			
 			t += dt;
 		}
 	}
